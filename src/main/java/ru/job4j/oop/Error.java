1d8a649987a8b487cb458aa373b7019a7f9f8d4c
nevsen4202 <nevsen2024@yandex.ru>
2024-10-01 19:04:20 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,36 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+        this.active = false;
+        this.status = 0;
+        this.message = "No error";
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void printInfo() {
+        System.out.println("Active: " + this.active);
+        System.out.println("Status: " + this.status);
+        System.out.println("Message: " + this.message);
+    }
+
+    public static void main(String[] args) {
+        Error defaultError = new Error();
+        defaultError.printInfo();
+
+        Error error1 = new Error(true, 404, "Not Found");
+        error1.printInfo();
+
+        Error error2 = new Error(false, 200, "OK");
+        error2.printInfo();
+    }
+}
