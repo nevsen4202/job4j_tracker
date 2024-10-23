8e6726ae6bc56c7de42a7ad33435842791f69efe
nevsen4202 <nevsen2024@yandex.ru>
2024-10-23 23:04:47 +0300

    1.6. Взаимодействие объектов [#166087 #515430]

--- src/main/java/ru/job4j/oop/BallStory/Fox.java
+++
@@ -1,7 +0,0 @@
-package ru.job4j.oop.BallStory;
-
-public class Fox {
-    public void tryEat(Ball ball) {
-        ball.tryRun(true);
-    }
-}
--- src/main/java/ru/job4j/oop/BallStory/Hare.java
+++
@@ -1,7 +0,0 @@
-package ru.job4j.oop.BallStory;
-
-public class Hare {
-    public void tryEat(Ball ball) {
-        ball.tryRun(false);
-    }
-}
